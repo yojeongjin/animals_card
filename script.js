2affc97c73fee1eb1783218c7a3904ec9ba39087
yojeongjin <osolku@naver.com>
2022-09-07 14:43:08 +0900

    CSS background-img 리팩토링 및 데이터코드 수정 refactor : css에서 처리하던 background-img를 javascript에서 처리하도록함 style : html element code 형식 수정
    1. css 내에서 :nth-child로 back class에 넣던 background-img를 javascript의 반복문으로 변경
    2. gameboard(=animals class)에 동물카드를 랜덤하게 배치하기 위해 반복문을 사용하여 수정함
    -
    1. html element data code의 형식을 01,02,03...에서 1,2,3...으로 수정하고 child element에도 dataset을 부여함
    2. e.target의 data code를 일치시키기 위함

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const $stage = document.querySelector('.stage')
 const $time = document.querySelector('.time')
 
+const animalsImg = ['whale', 'tiger', 'sheep', 'parrot', 'octopus', 'koala', 'hen', 'giraffe', 'frog', 'fox', 'flamingo', 'clownfish',
+'whale', 'tiger', 'sheep', 'parrot', 'octopus', 'koala', 'hen', 'giraffe', 'frog', 'fox', 'flamingo', 'clownfish']
+
 let stage = 1
 let time = 60
+let isOpen = false
 
 $stage.innerHTML = `STAGE ${stage}`
 
@@ -13,26 +17,22 @@ function timeout() {
     $time.innerHTML = `TIME ${time}`
   },1000) 
 }
+let animalList = [...document.querySelectorAll('.animal')]
 
-let $animal = document.querySelector('.animal')
-let $front = document.querySelector('.front')
-let $back = document.querySelector('.back')
+function shuffle(animalList) {
+  animalList.sort(() => Math.random() - 0.5);
+}
 
-
-const animalList = [...document.querySelectorAll('.animal')]
-
-console.log(animalList)
-
-function show() {
-  for(let i=0; i<24; i++) {
-    animalList[i].firstElementChild.style.transform = `rotateY(${180}deg)`
-    animalList[i].lastElementChild.style.transform = `rotateY(${0}deg)`
+function settingCard() {
+  for (let i=0; i<24; i++) {
+    animalList[i].lastElementChild.style.backgroundImage = `url(/assets/animals/${animalsImg[i]}.png)`
   }
 }
 
+
+
 function showBack() {
   let i = 0
-
   setInterval(function(){
     if (animalList[i]) {
       animalList[i].firstElementChild.style.transform = `rotateY(${180}deg)`
@@ -44,8 +44,8 @@ function showBack() {
     }
   },50)
   showFront()
+  // timeout()
 }
-showBack()
 
 function showFront() {
   setInterval(function() {
@@ -55,3 +55,25 @@ function showFront() {
     }
   },2000)
 }
+
+
+
+const $board = document.querySelector('.animals')
+$board.addEventListener('click', (e) => {
+  e.target.parentElement.firstElementChild.style.transform = `rotateY(${180}deg)`
+  e.target.parentElement.lastElementChild.style.transform = `rotateY(${0}deg)`
+
+  if (e.target.parentElement.dataset.value === e.target.dataset.value) {
+    console.log(e.target.dataset.value)
+  }
+
+})
+
+
+function gameStart() {
+  showBack()
+  shuffle(animalList)
+  settingCard()
+}
+
+window.onload = gameStart()
