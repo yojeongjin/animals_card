1b90d13a2360328d45e92e2ae5cebffc82b95871
yojeongjin <osolku@naver.com>
2022-09-07 16:00:11 +0900

    전체적인 코드 수정 style: 반복되는 html element 수정
    1. 반복되는 animal,front,back class들을 javascript에서 반복문으로  구현하여 html의 로직을 줄임
    2. 게임의 플레이를 위해서는 동물카드의 배치가 랜덤하게 이루어져야하는데,
    html에서 dataset을 이미 정의해버려서 random한 data-code와 value를 얻을 수 없었다.
    또한, 반복되는 class의 나열으로 인하여 로직의 효율성이 저하되는것을 막기위하여 수정

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const $stage = document.querySelector('.stage')
 const $time = document.querySelector('.time')
 
-const animalsImg = ['whale', 'tiger', 'sheep', 'parrot', 'octopus', 'koala', 'hen', 'giraffe', 'frog', 'fox', 'flamingo', 'clownfish',
-'whale', 'tiger', 'sheep', 'parrot', 'octopus', 'koala', 'hen', 'giraffe', 'frog', 'fox', 'flamingo', 'clownfish']
-
 let stage = 1
 let time = 60
 let isOpen = false
@@ -17,45 +14,65 @@ function timeout() {
     $time.innerHTML = `TIME ${time}`
   },1000) 
 }
-let animalList = [...document.querySelectorAll('.animal')]
 
-function shuffle(animalList) {
-  animalList.sort(() => Math.random() - 0.5);
+
+const animalsImg = 
+['whale', 'tiger', 'sheep', 'parrot', 'octopus', 'koala', 'hen', 'giraffe', 'frog', 'fox', 'flamingo', 'clownfish',
+'whale', 'tiger', 'sheep', 'parrot', 'octopus', 'koala', 'hen', 'giraffe', 'frog', 'fox', 'flamingo', 'clownfish']
+
+const $animals = document.querySelector('.animals')
+const $front = document.getElementsByClassName('front')
+const $back = document.getElementsByClassName('back')
+
+
+//카드 섞기
+function shuffle(animalsImg) {
+  animalsImg.sort(() => Math.random() - 0.5);
 }
 
+// 카드 셋팅
 function settingCard() {
+  shuffle(animalsImg)
   for (let i=0; i<24; i++) {
-    animalList[i].lastElementChild.style.backgroundImage = `url(/assets/animals/${animalsImg[i]}.png)`
+    $animals.innerHTML = $animals.innerHTML + `
+    <div class="animal" data-code="${i}" data-value="${animalsImg[i]}">
+      <div class="front"></div>
+      <div class="back"></div>
+    </div>`
+
+    $front[i].style.backgroundImage = `url(/assets/animals/zoo.png)`
+    $back[i].style.backgroundImage = `url(/assets/animals/${animalsImg[i]}.png)`
   }
 }
+settingCard()
 
-
-
+// 시작하면 뒷면 보여주고
 function showBack() {
   let i = 0
+  
   setInterval(function(){
-    if (animalList[i]) {
-      animalList[i].firstElementChild.style.transform = `rotateY(${180}deg)`
-      animalList[i].lastElementChild.style.transform = `rotateY(${0}deg)`
-      
-      i++
+    if(i<24) {
+      $front[i].style.transform = `rotateY(${180}deg)`
+      $back[i++].style.transform = `rotateY(${0}deg)`
+
     } else if (i>24) {
       clearInterval(showBack)
     }
   },50)
-  showFront()
-  // timeout()
-}
 
+  showFront()
+}
+showBack()
+
+// 다시 앞면
 function showFront() {
   setInterval(function() {
     for(let i=0; i<24; i++) {
-      animalList[i].firstElementChild.style.transform = `rotateY(${0}deg)`
-      animalList[i].lastElementChild.style.transform = `rotateY(${-180}deg)`
+      $front[i].style.transform = `rotateY(${0}deg)`
+      $back[i].style.transform = `rotateY(${-180}deg)`
     }
   },2000)
 }
-
 
 
 const $board = document.querySelector('.animals')
@@ -63,17 +80,10 @@ $board.addEventListener('click', (e) => {
   e.target.parentElement.firstElementChild.style.transform = `rotateY(${180}deg)`
   e.target.parentElement.lastElementChild.style.transform = `rotateY(${0}deg)`
 
-  if (e.target.parentElement.dataset.value === e.target.dataset.value) {
-    console.log(e.target.dataset.value)
-  }
+  console.log(e)
+
+  // if (e.target.parentElement.dataset.value === e.target.dataset.value) {
+  //   console.log(e.target.dataset.value)
+  // }
 
 })
-
-
-function gameStart() {
-  showBack()
-  shuffle(animalList)
-  settingCard()
-}
-
-window.onload = gameStart()
