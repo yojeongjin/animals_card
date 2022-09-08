045a743e681e0b0a8092839a5251ecc38cd3b1ad
yojeongjin <osolku@naver.com>
2022-09-08 11:12:44 +0900

    Click event 수정 fix: click event가 발생한 후  카드가 자동으로 뒤집히지 않도록 수정
    1. click event가 발생하면 카드의 뒷면(동물그림)을 보여주고 0.5초 후  다시 앞면이 보여지는 현상 수정
    2. 방금 발생한 click event의 타겟과  다음 click event의 타겟이  같은 타겟인지 확인하기 전까지는 카드가 뒤집히면 안되므로 수정함
    
    -

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ const $time = document.querySelector('.time')
 
 let stage = 1
 let time = 60
-let isOpen = false
 
 $stage.innerHTML = `STAGE ${stage}`
 
-function timeout() {
+function countdown() {
   setInterval(function() {
     console.log(time);
     time--;
@@ -20,7 +19,8 @@ const animalsImg =
 ['whale', 'tiger', 'sheep', 'parrot', 'octopus', 'koala', 'hen', 'giraffe', 'frog', 'fox', 'flamingo', 'clownfish',
 'whale', 'tiger', 'sheep', 'parrot', 'octopus', 'koala', 'hen', 'giraffe', 'frog', 'fox', 'flamingo', 'clownfish']
 
-const $animals = document.querySelector('.animals')
+const $board = document.querySelector('.animals')
+const $animal = document.getElementsByClassName('.animal')
 const $front = document.getElementsByClassName('front')
 const $back = document.getElementsByClassName('back')
 
@@ -34,7 +34,7 @@ function shuffle(animalsImg) {
 function settingCard() {
   shuffle(animalsImg)
   for (let i=0; i<24; i++) {
-    $animals.innerHTML = $animals.innerHTML + `
+    $board.innerHTML = $board.innerHTML + `
     <div class="animal" data-code="${i}" data-value="${animalsImg[i]}">
       <div class="front"></div>
       <div class="back"></div>
@@ -54,36 +54,65 @@ function showBack() {
     if(i<24) {
       $front[i].style.transform = `rotateY(${180}deg)`
       $back[i++].style.transform = `rotateY(${0}deg)`
-
-    } else if (i>24) {
-      clearInterval(showBack)
     }
   },50)
 
-  showFront()
+  // showFront()
+  // setTimeout(countdown, 2000)
 }
 showBack()
+
+// 카드를 다시 뒤집을까요?
+let isTurn = true
 
 // 다시 앞면
 function showFront() {
   setInterval(function() {
-    for(let i=0; i<24; i++) {
-      $front[i].style.transform = `rotateY(${0}deg)`
-      $back[i].style.transform = `rotateY(${-180}deg)`
+    if(isTurn){
+      for(let i=0; i<24; i++) {
+        $front[i].style.transform = `rotateY(${0}deg)`
+        $back[i].style.transform = `rotateY(${-180}deg)`
+      }
+    } else {
+      clearInterval(showFront)
     }
   },2000)
 }
 
+// 아직 뒤집지 마세요
+let stopFlip = false
 
-const $board = document.querySelector('.animals')
 $board.addEventListener('click', (e) => {
+  isTurn = false
+  if (stopFlip) {
+    return
+  }
+
   e.target.parentElement.firstElementChild.style.transform = `rotateY(${180}deg)`
   e.target.parentElement.lastElementChild.style.transform = `rotateY(${0}deg)`
-
-  console.log(e)
-
-  // if (e.target.parentElement.dataset.value === e.target.dataset.value) {
-  //   console.log(e.target.dataset.value)
-  // }
-
+  getValue(e.target.parentElement.dataset.value)
 })
+
+let openValue = []
+
+
+function getValue(value,target) {
+  openValue.push(value)
+
+
+  if (openValue.length === 2) {
+    stopFlip = true
+  }
+  flipCard(openValue)
+}
+
+function flipCard(openValue) {
+  let firstCard = openValue[0]
+  let secondCard = openValue [1]
+  
+  if (firstCard === secondCard) {
+    fitstTarget.classList.add('non-show')
+    secondTarget.classList.add('non-show')
+
+  } 
+}
